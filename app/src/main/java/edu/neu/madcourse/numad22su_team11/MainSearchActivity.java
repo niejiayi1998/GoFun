e2a4730931b71807b450079cd59d77f3103423be
Jiayi Nie <nie.jiay@northeastern.edu>
2022-08-04 14:48:05 -0700

    add location fetch from user

--- app/src/main/java/edu/neu/madcourse/numad22su_team11/MainSearchActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22su_team11/MainSearchActivity.java
@@ -1,21 +1,126 @@
 package edu.neu.madcourse.numad22su_team11;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainSearchActivity extends AppCompatActivity {
+
+    TextView tv_currLocation;
+    Button btn_update;
+    double latitude;
+    double longitude;
+
+    private static final int PERMISSIONS_FINE_LOCATION = 99;
+    private LocationRequest locationRequest;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_search);
+
+        latitude = 37.3387;
+        longitude = 121.8853;
+
+        tv_currLocation = findViewById(R.id.tv_currLocation);
+
+        createLocationRequest();
+        getCurrentLocation();
+
+
+        btn_update = findViewById(R.id.btn_updateLocation);
+        btn_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getCurrentLocation();
+            }
+        });
 
     }
 
     public void onClick(View view) {
         Intent intent = new Intent(MainSearchActivity.this, SearchDisplayActivity.class);
         startActivity(intent);
+    }
+
+    protected void createLocationRequest() {
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(Priority.PRIORITY_HIGH_ACCURACY);
+    }
+
+    private void getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.getFusedLocationProviderClient(this).requestLocationUpdates(locationRequest, new LocationCallback() {
+                @Override
+                public void onLocationResult(@NonNull LocationResult locationResult) {
+                    super.onLocationResult(locationResult);
+                    LocationServices.getFusedLocationProviderClient(MainSearchActivity.this).removeLocationUpdates(this);
+                    if (locationResult != null && locationResult.getLocations().size() >0){
+                        int index = locationResult.getLocations().size() - 1;
+                        latitude = locationResult.getLocations().get(index).getLatitude();
+                        longitude = locationResult.getLocations().get(index).getLongitude();
+                        Location location = locationResult.getLocations().get(index);
+                        try {
+                            updateUI(location);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            tv_currLocation.setText("Current Location Unavailable");
+                        }
+                    }
+                }
+                }, Looper.getMainLooper());
+        } else {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_FINE_LOCATION);
+        }
+    }
+
+    private void updateUI(Location location) throws IOException {
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+        String cityName = addresses.get(0).getLocality();
+        String stateName = addresses.get(0).getAdminArea();
+        String currentLocation = cityName + ", " + stateName;
+        tv_currLocation.setText(currentLocation);
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 99) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getCurrentLocation();
+            } else {
+                Toast.makeText(this, "No access to current location. Will use default location.", Toast.LENGTH_SHORT).show();
+                tv_currLocation.setText("San Jose, California");
+            }
+        }
     }
 }
