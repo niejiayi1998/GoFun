8e15d8bd5420d36b061e3f7591b6564bcb56209c
KAI <ares.jia@hotmail.com>
2022-08-07 02:03:34 -0700

    sign in -> finished

--- app/src/main/java/edu/neu/madcourse/numad22su_team11/StartActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22su_team11/StartActivity.java
@@ -1,18 +1,59 @@
 package edu.neu.madcourse.numad22su_team11;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class StartActivity extends AppCompatActivity {
 
+    private EditText ETEmail;
+    private EditText ETPassword;
+
+    FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+        ETEmail = findViewById(R.id.editTextEmail);
+        ETPassword = findViewById(R.id.editTextPassword);
+
+        // Connect with Firebase
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    public void signIn(View view) {
+        String email = ETEmail.getText().toString().trim();
+        String password = ETPassword.getText().toString();
+
+        if (email.equals("")) {
+            Toast.makeText(StartActivity.this, "Email can't be empty", Toast.LENGTH_SHORT).show();
+        } else {
+            firebaseAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Intent intent = new Intent(StartActivity.this, MainSearchActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                                finish();
+                            } else {
+                                Toast.makeText(StartActivity.this, "Wrong password or account doesn't exist!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
     }
 
     public void signUp(View view) {
--- app/src/main/java/edu/neu/madcourse/numad22su_team11/SuveryActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22su_team11/SuveryActivity.java
@@ -1,17 +1,88 @@
 package edu.neu.madcourse.numad22su_team11;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.neu.madcourse.numad22su_team11.Model.User;
 
 public class SuveryActivity extends AppCompatActivity {
+
+    private CheckBox CBHiking;
+    private CheckBox CBSports;
+    private CheckBox CBAmusement;
+    private CheckBox CBArts;
+    private CheckBox CBScenicView;
+    private CheckBox CBEntertainment;
+
+    DatabaseReference reference;
+    FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_suvery);
+
+        CBHiking = findViewById(R.id.checkBoxHiking);
+        CBSports = findViewById(R.id.checkBoxSports);
+        CBAmusement = findViewById(R.id.checkBoxAmusement);
+        CBArts = findViewById(R.id.checkBoxArts);
+        CBScenicView = findViewById(R.id.checkBoxScenicView);
+        CBEntertainment = findViewById(R.id.checkBoxEntertainment);
+
+        // connect with firebase
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+
+    }
+
+    // onclick method for letsGoFun button
+    public void letsGoFun(View view) {
+        List<Integer> preferences = new ArrayList<>();
+        if (CBHiking.isChecked()) preferences.add(1);
+        else preferences.add(0);
+        if (CBSports.isChecked()) preferences.add(1);
+        else preferences.add(0);
+        if (CBAmusement.isChecked()) preferences.add(1);
+        else preferences.add(0);
+        if (CBArts.isChecked()) preferences.add(1);
+        else preferences.add(0);
+        if (CBScenicView.isChecked()) preferences.add(1);
+        else preferences.add(0);
+        if (CBEntertainment.isChecked()) preferences.add(1);
+        else preferences.add(0);
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User currentUser = snapshot.getValue(User.class);
+                assert currentUser != null;
+                currentUser.setPreferences(preferences);
+                reference.setValue(currentUser);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        // startActivity(new Intent(SuveryActivity.this, MainSearchActivity.class));
     }
 
 
