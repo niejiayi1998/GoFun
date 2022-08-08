e53304b2581727a9f16a3a8e95d3cfaf12ca1b8a
Jiayi Nie <nie.jiay@northeastern.edu>
2022-08-07 21:49:55 -0700

    enable like checkbox function in LocationDetailActivity

--- app/src/main/java/edu/neu/madcourse/numad22su_team11/LocationDetailActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22su_team11/LocationDetailActivity.java
@@ -8,9 +8,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.material.checkbox.MaterialCheckBox;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -18,9 +20,10 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
-import org.w3c.dom.Text;
 
+import java.util.HashMap;
 import java.util.List;
 
 import edu.neu.madcourse.numad22su_team11.Adapter.EventAdapter;
@@ -42,6 +45,9 @@ public class LocationDetailActivity extends AppCompatActivity {
     private TextView tv_num_liked;
     private String locationId;
     private List<Event> eventList;
+    private List<String> likedBy;
+    private MaterialCheckBox checkBox;
+    private String userid;
 
     FirebaseUser firebaseUser;
     DatabaseReference eventDatabaseReference;
@@ -61,35 +67,66 @@ public class LocationDetailActivity extends AppCompatActivity {
         tv_locationDescription = findViewById(R.id.tv_description);
         tv_locationAddress = findViewById(R.id.tv_address);
         tv_num_liked = findViewById(R.id.tv_num_like);
+        checkBox = findViewById(R.id.checkBox);
 
 
         Bundle bundle = getIntent().getExtras();
         locationId = bundle.getString("locationId");
 
-        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        locationDatabaseReference = FirebaseDatabase.getInstance().getReference("Locations");
+        userid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        locationDatabaseReference = FirebaseDatabase.getInstance().getReference("Locations").child(locationId);
         locationDatabaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for (DataSnapshot dataSnapshot: snapshot.getChildren()){
-                    Location location = dataSnapshot.getValue(Location.class);
-                    if (location.getLocationId().equals(locationId)){
-                        locationName = location.getName();
-                        locationImgUrl = location.getImgUrl();
-                        locationAddress = location.getAddress();
-                        locationDescription = location.getDescription();
-                        locationLiked = location.getNumOfLike();
-                        tv_locationName.setText(locationName);
-                        tv_locationDescription.setText(locationDescription);
-                        tv_locationAddress.setText(locationAddress);
-                        tv_num_liked.setText(String.valueOf(locationLiked));
-                    }
+                Location location = snapshot.getValue(Location.class);
+                locationName = location.getName();
+                locationImgUrl = location.getImgUrl();
+                locationAddress = location.getAddress();
+                locationDescription = location.getDescription();
+                locationLiked = location.getNumOfLike();
+
+                likedBy = location.getLikedBy();
+
+                Picasso.get().load(locationImgUrl).into(iv_locationImg);
+                tv_locationName.setText(locationName);
+                tv_locationDescription.setText(locationDescription);
+                tv_locationAddress.setText(locationAddress);
+                tv_num_liked.setText(String.valueOf(locationLiked));
+
+                if (likedBy != null && likedBy.contains(userid)) {
+                    checkBox.setChecked(true);
                 }
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
+            }
+        });
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked && !likedBy.contains(userid)) {
+                    int numOfLike = Integer.valueOf(locationLiked) + 1;
+                    // update firebase locations locationId likedBy, add current user to list
+                    likedBy.add(userid);
+                    HashMap<String, Object> update = new HashMap<>();
+                    update.put("likedBy", likedBy);
+                    update.put("numOfLike", numOfLike);
+                    locationDatabaseReference.updateChildren(update);
+                    tv_num_liked.setText(String.valueOf(numOfLike));
+
+                } else if (!isChecked && likedBy.contains(userid)){
+                    int numOfLike = Integer.valueOf(locationLiked) - 1;
+                    // remove current user from likedBy
+                    likedBy.remove(userid);
+                    HashMap<String, Object> update = new HashMap<>();
+                    update.put("likedBy", likedBy);
+                    update.put("numOfLike", numOfLike);
+                    locationDatabaseReference.updateChildren(update);
+                    tv_num_liked.setText(String.valueOf(numOfLike));
+                }
             }
         });
 
