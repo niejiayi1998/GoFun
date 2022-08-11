4d102d28f9daae8955224ea9e8f7827278ecb314
daisychuying <58837611+daisychuying@users.noreply.github.com>
2022-08-11 11:19:50 -0700

    Updated User Profile Image on user Profile Page

--- app/src/main/java/edu/neu/madcourse/numad22su_team11/UserProfileActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22su_team11/UserProfileActivity.java
@@ -15,6 +15,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TextView;
 
